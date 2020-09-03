07e1c7da12f57675f6998d3c6caad07ad619d629
JM <wesierskij@gmail.com>
2020-09-03 12:44:36 -0700

    Initial Commit

---
+++ JGProject-JM-Wesierski/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JGProject-JM-Wesierski
+//
+//  Created by Jack Michael Wesierski on 9/3/20.
+//  Copyright © 2020 SFSU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
