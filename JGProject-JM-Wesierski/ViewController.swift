5dd5642c16b341dfd4a5aa122253644103aa1588
jwesierski <wesierskij@gmail.com>
2020-09-24 12:41:48 -0700

    Working with swift Arrays

--- JGProject-JM-Wesierski/ViewController.swift
+++ JGProject-JM-Wesierski/ViewController.swift
@@ -12,11 +12,32 @@ class ViewController: UIViewController {
     @IBOutlet weak var messageLabel: UILabel!
     @IBOutlet weak var imageView: UIImageView!
     
+    var imageNumber = 0
+    var messageNumber = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     @IBAction func messageButtonPressed(_ sender: UIButton) {
-        let awesomeMessage = "You Are Awesome!"
+        let messages = ["You Are Awesome!",
+                        "You Are Great!",
+                        "You Are Da Bomb!",
+                        "Fabulous? That's You!"]
+        
+        messageLabel.text = messages[messageNumber]
+        messageNumber += 1
+        if messageNumber == messages.count {
+            messageNumber = 0
+        }
+       // let imageName = "image" + String(imageNumber)
+        let imageName = "image\(imageNumber)"
+        imageView.image = UIImage(named: imageName);
+        imageNumber = imageNumber + 1
+        if imageNumber == 10{
+            imageNumber = 0
+        }
+        //
+      /*  let awesomeMessage = "You Are Awesome!"
         let greatMessage = "You Are Great!"
         let bombMessage = "You Are Da Bomb!"
         
@@ -29,7 +50,8 @@ class ViewController: UIViewController {
        } else {
         messageLabel.text = awesomeMessage
         imageView.image = UIImage(named: "image0")
-        }
+        }*/
+        
     }
 }
 
